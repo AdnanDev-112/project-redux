3b837822d398baae6fb987436ea882d984dee3f7
Adnan112-dev <79618300+Adnan112-dev@users.noreply.github.com>
2022-01-19 21:09:07 +0530

    .

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom'
 const Profile = () => {
     const navigate = useNavigate();
     const [userData, setUserData] = useState({})
+    const [profileImg, setProfileImg] = useState("");
 
     const callProfilePage = async () => {
         try {
@@ -20,6 +21,10 @@ const Profile = () => {
 
             const data = await res.json();
             setUserData(data);
+            setProfileImg(data.Profile[0].profileData[0].image)
+            // console.log(data.Profile[0].profileData[0].image);
+
+
 
             if (!res.status === 200) {
                 console.log("Error");
@@ -50,7 +55,7 @@ const Profile = () => {
             <h1>Profile here</h1>
             <h2>Name:  {userData.name}</h2>
             <h2>Email: {userData.email}</h2>
-            <img src={userData.Profile[0].profileData[0].image} alt="" />
+            <img src={profileImg} alt="" />
         </>
     )
 }
